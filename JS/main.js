6e88446ed3ecff073f8be95eb66579da3003a39b
tereschenkoolexa <sashapena1337@gmail.com>
2020-01-22 23:14:06 +0200

    Сreated a new class Crypto

--- JS/main.js
+++ JS/main.js
@@ -3,6 +3,16 @@ var baseUrl = "https://poloniex.com/public?command=returnCurrencies";
 api.open('GET', baseUrl, true);
 api.send();
 
+class Crypto{
+    constructor(id, name, humanType, depositAddress)
+    {
+        this.id = id;
+        this.name = name;
+        this.humanType = humanType;
+        this.depositAddress = depositAddress;
+    }
+}
+
 api.onload = function () {
     if (api.status == 200) {
         var data = JSON.parse(api.responseText);
