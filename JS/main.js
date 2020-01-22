e9599da376c0a080e51c14ba18b1fe7d0615f525
tereschenkoolexa <sashapena1337@gmail.com>
2020-01-23 01:08:09 +0200

    Сreated a new array

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,36 @@
-var api = new XMLHttpRequest();
-var baseUrl = "https://poloniex.com/public?command=returnCurrencies";
-api.open('GET', baseUrl, true);
-api.send();
-
-class Crypto{
-    constructor(id, name, humanType, depositAddress)
-    {
-        this.id = id;
-        this.name = name;
-        this.humanType = humanType;
-        this.depositAddress = depositAddress;
+var result =[];
+fetch("https://poloniex.com/public?command=returnCurrencies")
+  .then(
+    function (response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+      
+      response.json().then(function (data) {
+        result = Object.keys(data).map(function (key) {return data[key];})
+        console.log(result);
+      });
     }
-}
+  )
+  .catch(function (err) {
+    console.log('Fetch Error :-S', err);
+  });
 
-api.onload = function () {
-    if (api.status == 200) {
-        var data = JSON.parse(api.responseText);
-        console.log(data);
-
-    } else {
-        alert(`Error: ${api.status} code!`);
-    }
-}
+  for (var i = 0; i < data.articles.length; i++) {
+    $('body').append(
+        `
+        <div class="card bg-dark text-white" style="float: left; margin-top: 10px; width: 100%;">
+            <img class="card-img-top" src="${data.articles[i].urlToImage}" alt="Card image" style="width:100%">
+                <div class="card-body">
+                    <h4 class="card-title">${data.articles[i].author}</h4>
+                    <p class="card-text">${data.articles[i].title}</p>
+                    <p class="card-text">${data.articles[i].description}</p>
+                    <pre class="card-text">${data.articles[i].publishedAt}</pre>
+                    <a href="#" class="btn btn-light stretched-link">See Profile</a>
+                </div>
+        </div>
+        </div>
+        `
+    );}
