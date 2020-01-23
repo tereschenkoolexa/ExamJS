5d219eef03b777a116808d0ae029d42a98713ca5
tereschenkoolexa <sashapena1337@gmail.com>
2020-01-23 02:09:36 +0200

    End of work

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,27 @@
-var result =[];
+var result = [];
+
+function Delete(index) {
+  console.log(result)
+  if (index > -1) {
+    result.splice(index, 1);
+  }
+  console.log(result)
+  $(".tableData").html("");
+  for (var i = 0; i < result.length; i++) {
+    $('.tableData').append(
+      `
+        <tr>
+                <td>${result[i].id}</td>
+                <td>${result[i].name}</td>
+                <td>${result[i].humanType}</td>
+                <td><button class="btn btn-dark" onclick="Delete(${result.indexOf(result[i])})" type="submit"><i class="far fa-minus-square"></i></button></td>
+        </tr>
+        `
+    );
+  }
+}
+
+
 fetch("https://poloniex.com/public?command=returnCurrencies")
   .then(
     function (response) {
@@ -7,30 +30,25 @@ fetch("https://poloniex.com/public?command=returnCurrencies")
           response.status);
         return;
       }
-      
+
       response.json().then(function (data) {
-        result = Object.keys(data).map(function (key) {return data[key];})
+        result = Object.keys(data).map(function (key) { return data[key]; })
         console.log(result);
+        for (var i = 0; i < result.length; i++) {
+          $('.tableData').append(
+            `
+              <tr>
+                      <td>${result[i].id}</td>
+                      <td>${result[i].name}</td>
+                      <td>${result[i].humanType}</td>
+                      <td><button class="btn btn-dark" onclick="Delete(${result.indexOf(result[i])})" type="submit"><i class="far fa-minus-square"></i></button></td>
+              </tr>
+              `
+          );
+        }
       });
     }
   )
   .catch(function (err) {
     console.log('Fetch Error :-S', err);
   });
-
-  for (var i = 0; i < data.articles.length; i++) {
-    $('body').append(
-        `
-        <div class="card bg-dark text-white" style="float: left; margin-top: 10px; width: 100%;">
-            <img class="card-img-top" src="${data.articles[i].urlToImage}" alt="Card image" style="width:100%">
-                <div class="card-body">
-                    <h4 class="card-title">${data.articles[i].author}</h4>
-                    <p class="card-text">${data.articles[i].title}</p>
-                    <p class="card-text">${data.articles[i].description}</p>
-                    <pre class="card-text">${data.articles[i].publishedAt}</pre>
-                    <a href="#" class="btn btn-light stretched-link">See Profile</a>
-                </div>
-        </div>
-        </div>
-        `
-    );}
